4a807f79dcfca39b2da2b073e487a50cbaf8c488
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 09:09:11 +0100

    feat: GetAccountRequest

--- src/main/java/org/example/dto/request/GetAccountRequest.java
+++ src/main/java/org/example/dto/request/GetAccountRequest.java
@@ -1,4 +1,17 @@
 package org.example.dto.request;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GetAccountRequest {
+
+    private String accountId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
 }
+
