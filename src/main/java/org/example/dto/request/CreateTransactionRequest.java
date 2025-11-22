8333bbac42741b9de234deaed20f40db590dab66
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 09:05:37 +0100

    feat: CreateAddressRequest

--- src/main/java/org/example/dto/request/CreateTransactionRequest.java
+++ src/main/java/org/example/dto/request/CreateTransactionRequest.java
@@ -1,4 +1,11 @@
 package org.example.dto.request;
 
+import org.example.data.model.TransactionType;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
+
 public class CreateTransactionRequest {
+    private String accountId;
+    private TransactionType transactionType;
+    private TransactionStatus transactionStatus;
+    private double amount;
 }
