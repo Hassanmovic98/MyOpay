b3c68c7340f3c5e645bca907bea1511410eddfeb
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 09:06:29 +0100

    feat: CreateAccountResponse

--- src/main/java/org/example/dto/response/GetAccountResponse.java
+++ src/main/java/org/example/dto/response/GetAccountResponse.java
@@ -1,4 +1,14 @@
 package org.example.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class GetAccountResponse {
+    private String Message;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String accountNumber;
 }
