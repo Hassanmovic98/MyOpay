f060054a88557222295f4b2d4cecd62c49cfe71b
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 09:03:29 +0100

    feat: AccountRepository

--- src/main/java/org/example/data/repository/AccountRepository.java
+++ src/main/java/org/example/data/repository/AccountRepository.java
@@ -3,7 +3,19 @@ package org.example.data.repository;
 import org.example.data.model.Account;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface AccountRepository extends MongoRepository<Account, String> {
+
+    Optional<Account> findByEmailAddress(String emailAddress);
+
+    Optional<Account> findByFirstName(String firstName);
+
+    Optional<Account> findByLastName(String lastName);
+
+    List<Account> findByFirstNameAndLastName(String firstName, String lastName);
+
 
 
 }
