77a57c7f10ca8db2e696ec1e324e9467f6a7a72d
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 09:32:18 +0100

    feat: TransactionImplementation

--- src/main/java/org/example/service/serviceImplementation/TransactionImplementation.java
+++ src/main/java/org/example/service/serviceImplementation/TransactionImplementation.java
@@ -1,9 +1,43 @@
 package org.example.service.serviceImplementation;
 
+import org.example.dto.request.CreateTransactionRequest;
+import org.example.dto.request.GetTransactionRequest;
+import org.example.dto.response.CreateAddressResponse;
+import org.example.dto.response.GetTransactionResponse;
 import org.example.service.serviceInterface.TransactionInterface;
+
+import java.util.List;
 
 public class TransactionImplementation implements TransactionInterface {
 
 
+    @Override
+    public CreateAddressResponse createTransaction(CreateTransactionRequest createTransactionRequest) {
+        return null;
+    }
 
+    @Override
+    public GetTransactionResponse getTransactionById(GetTransactionRequest getTransactionRequest) {
+        return null;
+    }
+
+    @Override
+    public GetTransactionResponse getTransactionByTransactionId(GetTransactionRequest getTransactionRequest) {
+        return null;
+    }
+
+    @Override
+    public List<GetTransactionResponse> getTransactionsByAccountId(GetTransactionRequest getTransactionRequest) {
+        return List.of();
+    }
+
+    @Override
+    public List<GetTransactionResponse> getTransactionsByTransactionIdAndType(GetTransactionRequest getTransactionRequest) {
+        return List.of();
+    }
+
+    @Override
+    public List<GetTransactionResponse> getTransactionsByAccountIdAndStatus(GetTransactionRequest getTransactionRequest) {
+        return List.of();
+    }
 }
