4eb8d6fc666ce0acf30f3947bf8d0ebcffec1227
hassan <salakohassanmobolaji@gmail.com>
2025-11-22 08:26:13 +0100

    feat: TransactionRepository & TransactionInterface

--- src/main/java/org/example/data/repository/TransactionRepository.java
+++ src/main/java/org/example/data/repository/TransactionRepository.java
@@ -1,7 +1,16 @@
 package org.example.data.repository;
 
 import org.example.data.model.Transaction;
+import org.example.data.model.TransactionStatus;
+import org.example.data.model.TransactionType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TransactionRepository extends MongoRepository<Transaction, String> {
+    List<Transaction> findByAccountId(String accountId);
+    List<Transaction> findByAccountIdAndTransactionType(String accountId, TransactionType transactionType);
+    List<Transaction> findByAccountIdAndTransactionStatus(String accountId, TransactionStatus transactionStatus);
+    Optional<Transaction> findByTransactionId(int transactionId);
 }
--- src/main/java/org/example/service/serviceInterface/TransactionInterface.java
+++ src/main/java/org/example/service/serviceInterface/TransactionInterface.java
@@ -1,9 +1,19 @@
 package org.example.service.serviceInterface;
 
-import org.example.dto.CreateTransactionResponse;
+
 import org.example.dto.request.CreateTransactionRequest;
+import org.example.dto.request.GetTransactionRequest;
+import org.example.dto.response.CreateAddressResponse;
+import org.example.dto.response.GetTransactionResponse;
+
+import java.util.List;
 
 public interface TransactionInterface {
-CreateTransactionResponse createTransaction (CreateTransactionRequest createTransactionRequest );
+    CreateAddressResponse createTransaction (CreateTransactionRequest createTransactionRequest);
+    GetTransactionResponse getTransactionById (GetTransactionRequest getTransactionRequest);
+    GetTransactionResponse getTransactionByTransactionId (GetTransactionRequest getTransactionRequest);
+    List<GetTransactionResponse> getTransactionsByAccountId (GetTransactionRequest getTransactionRequest);
+    List<GetTransactionResponse> getTransactionsByTransactionIdAndType (GetTransactionRequest getTransactionRequest);
+    List<GetTransactionResponse> getTransactionsByAccountIdAndStatus (GetTransactionRequest getTransactionRequest);
 
 }
