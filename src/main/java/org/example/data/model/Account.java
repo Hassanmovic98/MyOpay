8b1e13c173c9abe3130c1fd82e5a58cc0a56172e
hassan <salakohassanmobolaji@gmail.com>
2025-11-30 08:20:39 +0100

    Addition:added status,activation,deactivation to Account

--- src/main/java/org/example/data/model/Account.java
+++ src/main/java/org/example/data/model/Account.java
@@ -3,7 +3,10 @@ package org.example.data.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -11,13 +14,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Account {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String accountNumber;
     private String firstName;
     private String lastName;
     private Gender gender;
 
     private String addressId;
+    @Indexed(unique = true)
     private String emailAddress;
     private String phoneNumber;
     private int age;
+    private AccountStatus status =AccountStatus.ACTIVE;
+
+    private String deactivationReason;
+    private LocalDateTime deactivatedAt;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
