6c18ec556de80f69f424051d4f4c0349da1ab343
hassan <salakohassanmobolaji@gmail.com>
2025-12-01 15:41:41 +0100

    Additional of  DTos

---
+++ src/main/java/org/example/dto/response/UnsuspendAccountResponse.java
@@ -0,0 +1,17 @@
+package org.example.dto.response;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class UnsuspendAccountResponse {
+    private String message;
+    private String accountNumber;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String status;
+}
